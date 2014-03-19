45062f549da07dab47a7cac94cf0afb36e016b04
Philippe Dionne <dionne.phil@gmail.com>
2014-03-19 09:06:06 -0400

    Configure specs

---
+++ spec/spec_helper.rb
@@ -0,0 +1,14 @@
+require 'coveralls'
+Coveralls.wear!
+
+require 'bundler/setup'
+require 'rspec'
+
+require 'twin'
+
+RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.filter_run focus: true
+  config.filter_run_excluding skip: true
+  config.run_all_when_everything_filtered = true
+end
