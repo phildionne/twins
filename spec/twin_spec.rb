e76f321bc450bc19ac83a2068c8642fd35e7e2d1
Philippe Dionne <dionne.phil@gmail.com>
2014-03-20 11:35:18 -0400

    Add specs

---
+++ spec/twin_spec.rb
@@ -0,0 +1,105 @@
+require 'spec_helper'
+
+describe Twin do
+
+  describe :consolidate do
+
+    describe "with an empty collection" do
+      let(:collection) { [] }
+      it { expect(Twin.consolidate(collection)).to eq(nil) }
+    end
+
+    describe "with a collection of all same elements" do
+      let(:collection) { [{ a: 'a' }, { a: 'a' }, { a: 'a' }] }
+      it { expect(Twin.consolidate(collection)).to eq({ a: 'a' }) }
+    end
+
+    describe "with a collection of all different elements" do
+      let(:collection) { [{ a: 'a' }, { b: 'b' }, { c: 'c' }] }
+      it { expect(Twin.consolidate(collection)).to eq({ a: 'a', b: 'b', c: 'c' }) }
+    end
+
+    describe "with a collection of same and different elements" do
+      let(:collection) { [{ a: 'a', b: 'b' }, { a: 'a', b: 'b' }, { a: 'a', b: 'b', c: 'c' }] }
+      it { expect(Twin.consolidate(collection)).to eq({ a: 'a', b: 'b', c: 'c' }) }
+    end
+
+    describe "with nested hashes" do
+      let(:collection) do
+        [{ a: 'a',
+            b: {
+              x: 'x',
+              y: 'y'
+            }
+          },
+          { a: 'a',
+            b: {
+              x: 'x',
+              y: 'y'
+            }
+          }]
+      end
+      it { expect(Twin.consolidate(collection)).to eq({ a: 'a', b: { x: 'x', y: 'y' }}) }
+    end
+
+    context "with options" do
+
+      describe :priority do
+
+        describe "a String" do
+          let(:options) { { priority: { element: 'something' } } }
+
+          describe "with a single match" do
+            let(:collection) { [{ element: 'something' }, { element: 'something' }] }
+            it { expect(Twin.consolidate(collection, options)).to eq({ element: 'something' }) }
+          end
+
+          describe "with multiple matches" do
+            let(:collection) { [{ element: 'some' }, { element: 'thing' }, { element: 'something' }] }
+            it { expect(Twin.consolidate(collection, options)).to eq({ element: 'something' }) }
+          end
+
+          describe "with no match" do
+            let(:collection) { [{ element: 'thing1' }, { element: 'thing2' }] }
+            it { expect(Twin.consolidate(collection, options)).to eq({ element: 'thing1' }) }
+          end
+        end
+
+        describe "a Numeric" do
+          let(:options) { { priority: { element: 10 } } }
+
+          describe "with no difference and no mode" do
+            let(:collection) { [{ element: 10 }] }
+            it { expect(Twin.consolidate(collection, options)).to eq({ element: 10 }) }
+          end
+
+          describe "with no difference and a mode" do
+            let(:collection) { [{ element: 10 }, { element: 10 }] }
+            it { expect(Twin.consolidate(collection, options)).to eq({ element: 10 }) }
+          end
+
+          describe "with a higher difference and no mode" do
+            let(:collection) { [{ element: 1 }, { element: 2 }] }
+            it { expect(Twin.consolidate(collection, options)).to eq({ element: 2 }) }
+          end
+
+          describe "with a higher difference and a mode" do
+            let(:collection) { [{ element: 1 }, { element: 1 }, { element: 2 }] }
+            it { expect(Twin.consolidate(collection, options)).to eq({ element: 2 }) }
+          end
+
+          describe "with a lower difference and no mode" do
+            let(:collection) { [{ element: 1 }, { element: 2 }] }
+            it { expect(Twin.consolidate(collection, options)).to eq({ element: 2 }) }
+          end
+
+          describe "with a lower difference and a mode" do
+            let(:collection) { [{ element: 1 }, { element: 2 }, { element: 2 }] }
+            it { expect(Twin.consolidate(collection, options)).to eq({ element: 2 }) }
+          end
+        end
+      end
+    end
+
+  end
+end
